28bda49b27bd608a70b7bdcc1bdd05d8a2d5d400
Emiko Esther S <graceupongraceemi@gmail.com>
2025-01-21 21:55:44 -0800

    React Native styling added to App.js

--- App.js
+++ App.js
@@ -4,19 +4,20 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Another piece of text!</Text>
+        <Text style={styles.dummyText}>Another piece of text!</Text>
       </View>
       <Text
-        style={{
-          margin: 16,
-          borderwidth: 1,
-          borderColor: 'peach',
-          padding: 16
-        }}
+        style={
+          styles.dummyText
+        }
       >
-        Hello World!
+        Another place of text!
       </Text>
-      <Button title='Tap me' />
+    
+    <Text style={styles.dummyText}>
+      Hello World!
+    </Text>
+    <Button title='tap me!' />
     </View>
   );
 }
@@ -27,5 +28,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
+  }
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: 'blue',
   }
 });
