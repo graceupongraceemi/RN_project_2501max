f32784a42db15cb6668383b46b59a23f3793c2bd
Emiko Esther S <graceupongraceemi@gmail.com>
2025-01-27 20:16:11 -0800

    Flatlist revised with data and renterItem added

--- App.js
+++ App.js
@@ -37,13 +37,17 @@ export default function App() {
         <Button title='Add Goal' onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Flatlist alwaysBounceVertical={false}>
-          {courseGoals.map((goal) => (
-            <View key={goal} style={styles.goalItem}>
-              <Text style={styles.goalText}>{goal}</Text>
-            </View>
-          ))}
-        </Flatlist>
+        <Flatlist
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item}</Text>
+              </View>
+            );
+          }}
+          alwaysBounceVertical={false}
+        />
       </View>
     </View>
   );
