d0b5c77334b952e70d68e11cb19b9a509b25a125
Emiko Esther S <graceupongraceemi@gmail.com>
2025-02-02 21:10:39 -0800

    key in setCourseGoals function replaced with id. keyExtractor added to FlatList

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ export default function App() {
     // console.log(enteredGoalText);`
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
-      { text: enteredGoalText, key: Math.random().toString() }
+      { text: enteredGoalText, id: Math.random().toString() }
     ]);
   }
 
@@ -47,6 +47,9 @@ export default function App() {
                 <Text style={styles.goalText}>{itemData.item.text}</Text>
               </View>
             );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
           }}
           alwaysBounceVertical={false}
         />
