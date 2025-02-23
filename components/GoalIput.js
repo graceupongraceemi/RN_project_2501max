a78ebbdfaf4d2d06b415428c2c411b81d7cebdcb
Emiko Esther S <graceupongraceemi@gmail.com>
2025-02-22 18:39:00 -0800

    textInput and button color modified in GoalInput.js

--- components/GoalIput.js
+++ components/GoalIput.js
@@ -36,10 +36,10 @@ function GoalInput(props) {
         />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title='Add Goal' onPress={addGoalHandler} color='#b180f0' />
+            <Button title='Cancel' onPress={props.onCancel} color='#f31282' />
           </View>
           <View style={styles.button}>
-            <Button title='Cancel' onPress={props.onCancel} color='#f31282' />
+            <Button title='Add Goal' onPress={addGoalHandler} color='#b180f0' />
           </View>
         </View>
       </View>
@@ -65,10 +65,13 @@ const styles = StyleSheet.create({
   },
   textInput: {
     borderWith: 1,
-    borderColor: '#cccccc',
+    borderColor: '#e4d0ff',
+    backgroundColor: '#e4d0ff',
+    color: '#120438',
+    borderRadius: 6,
     width: '100%',
     // marginRight: 8,
-    padding: 8
+    padding: 16
   },
   buttonContainer: {
     marginTop: 16,
